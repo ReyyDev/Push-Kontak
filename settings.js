c20b9a972c2edcc584fcf242485dbe11e758c92b
ReyyDev <117353741+ReyyDev@users.noreply.github.com>
2024-03-08 16:32:24 +0700

    settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
 
-global.owner = ["62887435047326"]
+global.owner = ["6285859738152"]
 global.push_kontak_delay = 5000 //5 detik, 1000 = 1 detik
-global.pairingNumber = ""
+global.pairingNumber = "6285755859106"
